57070f3f8a2ef785edf4f6ac921b02f36c613813
Ephantus2 <ephantusmwago6@gmail.com>
2026-06-25 18:48:55 +0300

    final

--- server/cash/cash/settings.py
+++ server/cash/cash/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d4+^3+6io+_2a=uuz*k75-=-+^h3&^ib778t#f6y2zaw&u!&lb'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
