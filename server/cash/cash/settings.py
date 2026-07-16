3686b5e433fe045d76d6bb7ba65c65db57e4376c
Ephantus2 <ephantusmwago6@gmail.com>
2026-07-16 10:03:52 +0300

    update settings

--- server/cash/cash/settings.py
+++ server/cash/cash/settings.py
@@ -225,3 +225,14 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'ephantusmwago6@gmail.com'
+EMAIL_HOST_PASSWORD = 'cciw yczy svsd dszf'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
