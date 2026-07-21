10bfc0c48772563e3e355f261b950bba71895f0f
Ephantus2 <ephantusmwago6@gmail.com>
2026-07-21 08:49:22 +0300

    admin manage users

--- server/cash/accounts/admin.py
+++ server/cash/accounts/admin.py
@@ -1,6 +1,70 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import ReferralReward
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "phone_number",
+        "points",
+        "user_wallet",
+        "activated",
+        "is_staff",
+        "is_active",
+    )
+
+    list_filter = (
+        "activated",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+        "phone_number",
+        "referral_code",
+    )
+
+    readonly_fields = ("referral_code",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Referral Information",
+            {
+                "fields": (
+                    "referral_code",
+                    "referred_by",
+                    "points",
+                    "user_wallet",
+                    "from_referrals",
+                    "life_time_earning",
+                    "phone_number",
+                    "activated",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional Information",
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "phone_number",
+                    "activated",
+                ),
+            },
+        ),
+    )
 
 
 
